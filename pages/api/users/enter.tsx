f8fbcbc4b082004e453142a8b48f82fd54129d58
donghoonlee94 <94donghoon.lee@gmail.com>
2022-12-20 14:14:55 +0900

    add: auth email logic with sendgrid/mail

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,10 @@
+import mail from "@sendgrid/mail";
 import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 
+mail.setApiKey(process.env.SENDGRID_KEY!);
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 
@@ -37,7 +39,17 @@ async function handler(
       body: `Your login token is ${payload}.`,
     });
     console.log('message', message);
+  } else if (email) {
+    const email = await mail.send({
+      from: "94donghoon.lee@gmail.com",
+      to: "94donghoon.lee@gmail.com",
+      subject: "Your Carrot Market Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email);
   }
+
   return res.json({
     ok: true,
   });
