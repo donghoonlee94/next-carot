1f25ec6e4747a7d28b11547e6e8280cf93d1070d
donghoonlee94 <94donghoon.lee@gmail.com>
2022-12-20 15:06:52 +0900

    add: user/confirm API & token ondelete option cascade with prisma

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -33,21 +33,21 @@ async function handler(
     },
   });
   if (phone) {
-    const message = await twilioClient.messages.create({
-      messagingServiceSid: process.env.TWILIO_MSID,
-      to: process.env.MY_PHONE!,
-      body: `Your login token is ${payload}.`,
-    });
-    console.log('message', message);
+    // const message = await twilioClient.messages.create({
+    //   messagingServiceSid: process.env.TWILIO_MSID,
+    //   to: process.env.MY_PHONE!,
+    //   body: `Your login token is ${payload}.`,
+    // });
+    // console.log('message', message);
   } else if (email) {
-    const email = await mail.send({
-      from: "94donghoon.lee@gmail.com",
-      to: "94donghoon.lee@gmail.com",
-      subject: "Your Carrot Market Verification Email",
-      text: `Your token is ${payload}`,
-      html: `<strong>Your token is ${payload}</strong>`,
-    });
-    console.log(email);
+    // const email = await mail.send({
+    //   from: "94donghoon.lee@gmail.com",
+    //   to: "94donghoon.lee@gmail.com",
+    //   subject: "Your Carrot Market Verification Email",
+    //   text: `Your token is ${payload}`,
+    //   html: `<strong>Your token is ${payload}</strong>`,
+    // });
+    // console.log(email);
   }
 
   return res.json({
