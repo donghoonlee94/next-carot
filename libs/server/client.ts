49f2e0392ee46d10e12f0cfeef9ec1a3514fb526
donghoonlee94 <94donghoon.lee@gmail.com>
2022-12-21 10:31:23 +0900

    add: react hook form to upload.tsx

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -4,6 +4,7 @@ declare global {
     var client: PrismaClient | undefined;
 }
 
+// 클라이언트가 여러번 중복 생성되는 이슈가 있음.
 const client = global.client || new PrismaClient();
 
 if (process.env.NODE_ENV === "development") global.client = client;
