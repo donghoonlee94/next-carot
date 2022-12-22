362df943a5d7727a7e10cdd2f1901b84ec0b9372
donghoonlee94 <94donghoon.lee@gmail.com>
2022-12-23 00:56:07 +0900

    add: fav model, fav api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
-import type { NextPage } from "next";
-import FloatingButton from "@components/floating-button";
-import Item from "@components/item";
-import Layout from "@components/layout";
-import useUser from "@libs/client/useUser";
-import Head from "next/head";
+import type { NextPage } from 'next';
+import FloatingButton from '@components/floating-button';
+import Item from '@components/item';
+import Layout from '@components/layout';
+import useUser from '@libs/client/useUser';
+import Head from 'next/head';
 import useSWR from 'swr';
-import { Product } from "@prisma/client";
+import { Product } from '@prisma/client';
 
 interface ProductsResponse {
   ok: boolean;
-  products: Product[]
+  products: Product[];
 }
 
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
-  const { data } = useSWR<ProductsResponse>("/api/products");
+  const { data } = useSWR<ProductsResponse>('/api/products');
 
   return (
     <Layout title="홈" hasTabBar>
@@ -23,14 +23,7 @@ const Home: NextPage = () => {
       </Head>
       <div className="flex flex-col space-y-5 divide-y">
         {data?.products?.map((product) => (
-          <Item
-            id={product.id}
-            key={product.id}
-            title={product.name}
-            price={product.price}
-            comments={1}
-            hearts={1}
-          />
+          <Item id={product.id} key={product.id} title={product.name} price={product.price} hearts={1} />
         ))}
         <FloatingButton href="/products/upload">
           <svg
@@ -41,12 +34,7 @@ const Home: NextPage = () => {
             stroke="currentColor"
             aria-hidden="true"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-            />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
           </svg>
         </FloatingButton>
       </div>
