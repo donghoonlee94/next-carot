ae5afbafb9b775ff3287a6b7ade0bafc74906b00
donghoonlee94 <94donghoon.lee@gmail.com>
2022-12-26 12:24:31 +0900

    add: wonder api

--- pages/api/posts/[id]/index.ts
+++ pages/api/posts/[id]/index.ts
@@ -1,11 +1,15 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import withHandler, { ResponseType } from '@libs/server/withHandler';
-import client from '@libs/server/client';
-import { withApiSession } from '@libs/server/withSession';
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
 
-async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const {
     query: { id },
+    session: { user },
   } = req;
   const post = await client.post.findUnique({
     where: {
@@ -40,15 +44,27 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
       },
     },
   });
+  const isWondering = Boolean(
+    await client.wondering.findFirst({
+      where: {
+        postId: +id.toString(),
+        userId: user?.id,
+      },
+      select: {
+        id: true,
+      },
+    })
+  );
   res.json({
     ok: true,
     post,
+    isWondering,
   });
 }
 
 export default withApiSession(
   withHandler({
-    methods: ['GET'],
+    methods: ["GET"],
     handler,
   })
 );
