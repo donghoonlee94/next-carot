461ba27e271709d6fc01be2f547013355fd690a8
donghoonlee94 <94donghoon.lee@gmail.com>
2022-12-27 14:33:20 +0900

    change: middleware logic

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,29 @@
-import type { NextRequest } from 'next/server'
+import { NextRequest, NextResponse, userAgent } from 'next/server'
 
-export function middleware(request: NextRequest) {
-    console.log('global middle ware')
-    if (request.nextUrl.pathname.startsWith('/chats')) {
-        // This logic is only applied to /about
-        console.log('chats middle ware')
+export function middleware(req: NextRequest) {
+    const { isBot } = userAgent(req);
+    const { origin } = req.nextUrl;
+    if (isBot) {
+        return new Response("Plz don't be a bot. Be human.", { status: 403 });
     }
+
+    if (!req.url.includes("/api")) {
+        if (!req.url.includes("/enter") && !req?.cookies?.carrotsession) {
+            const url = req.nextUrl.clone()
+            url.pathname = '/enter'
+            return NextResponse.redirect(`${url}`)
+        }
+    }
+}
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|_next/static|favicon.ico).*)',
+    ],
 }
